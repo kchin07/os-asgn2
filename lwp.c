e66b98f841a8766cc6aaa9d0267185d08063f66c
kchin07 <kchin07@unix3.csc.calpoly.edu>
2018-10-10 17:15:44 -0700

    added his given files and made a lwp.c

---
+++ lwp.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "lwp.h"
+
+tid_t lwp_create(lwpfun func, void* ptr, size_t size){
+
+}
+
+void lwp_exit(){
+
+}
+
+tid_t lwp_gettid(){
+
+}
+
+void lwp_yield(){
+
+}
+
+void lwp_start(){
+
+}
+
+void lwp_stop(){
+
+}
+
+void lwp_set_scheduler(scheduler fun){
+
+}
+
+thread tid2thred(tid_t tid){
+
+}
